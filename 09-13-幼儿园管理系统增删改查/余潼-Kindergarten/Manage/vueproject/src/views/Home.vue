408f0936512ba71c251777b5003098594d4d52c0
unknown <1909044270@qq.com>
2020-09-14 20:08:44 +0800

    成都托儿所管理系统

---
+++ 09-13-幼儿园管理系统增删改查/余潼-Kindergarten/Manage/vueproject/src/views/Home.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="home">
+    <el-container>
+      <el-header>
+        <el-row>
+          <el-col :span="6">
+            <h3>成都托儿所国际幼儿园管理平台</h3>
+          </el-col>
+          <el-col :span="12">
+          </el-col>
+          <el-col :span="6">
+            <p>xiaotong</p>
+          </el-col>
+        </el-row>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">
+          <LeftMenu/>
+        </el-aside>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
+    <el-footer>
+      <p>2020 * 成都托儿所国际幼儿园管理平台</p>
+    </el-footer>
+  </div>
+</template>
+
+<script>
+import LeftMenu from "./../components/LeftMenu.vue";
+export default {
+  name: "Home",
+  components: {
+    LeftMenu
+  }
+};
+</script>
+<style scoped>
+*{
+  margin: 0;
+  padding: 0;
+}
+.el-header, .el-footer {
+  background-color: #0f4E74;
+  color: white;
+  height: 60px;
+}
+
+.el-header .el-col {
+  height: 60px;
+  line-height: 60px;
+}
+.el-header .el-col p{
+text-align: right;
+}
+.el-footer{
+  text-align: center;
+  height: 60px;
+  line-height: 60px;
+}
+.el-aside {
+  background-color: #D3DCE6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  min-height: 600px;
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+</style>
---
+++ 09-13-幼儿园管理系统增删改查/余潼-Kindergarten/Manage/vueproject/src/views/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>欢迎来到成都托儿所国际幼儿园管理平台</h2>
+    <p>你可以进行相关的操作</p>
+    <p>图表</p>
+    <p>一共职工1000人</p>
+    <p>学生20000人</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "Index",
+  data: function () {
+    return {}
+  }
+}
+</script>
+<style scoped>
+</style>
