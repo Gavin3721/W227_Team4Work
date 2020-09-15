86ec783e70e03a6fd269c42b3ddf493f0d126cab
paddyLiuLian <y3497917491@qq.com>
2020-09-15 09:19:31 +0800

    vue

---
+++ 09-13-幼儿园管理系统增删改查/刘炼/src/views/Index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h2>欢迎来到轻化工幼儿园管理员</h2>
+      <p>你可以进行相关的操作</p>
+      <p>图表</p>
+      <p>一共职工100人</p>
+      <p>学生999个人</p>
+    </div>
+</template>
+<script>
+export default {
+  name: 'Index',
+  data: function () {
+    return {}
+  }
+}
+</script>
+<style scoped>
+</style>
